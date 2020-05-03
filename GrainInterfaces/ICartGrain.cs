b18e121fd8b29b2f12a8abe7be23457986cb13b2
Miguel Cardoso <msdc.1000@hotmail.com>
2020-05-03 21:12:21 +0200

    Adding un-tested cart controller. Will define model and test it out later. The endpoints will probably be something like this.

--- GrainInterfaces/ICartGrain.cs
+++ GrainInterfaces/ICartGrain.cs
@@ -6,14 +6,11 @@ using System.Threading.Tasks;
 namespace OrleansBasics
 {
     public interface ICartGrain : IGrainWithGuidKey
-
-    {   //From which user?
+    { 
         Task<Cart> GetCart();
 
-        //From which user?
         Task<List<Product>> GetProducts();
 
-        //To which user?
         Task AddProduct(Product product);
     }
 }
