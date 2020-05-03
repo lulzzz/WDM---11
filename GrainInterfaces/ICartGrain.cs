37a6a285a69fdcab4d8786c257f44c3bc139260e
Miguel Cardoso <msdc.1000@hotmail.com>
2020-05-03 17:23:03 +0200

    Playing around with Orleans and initial setup

---
+++ GrainInterfaces/ICartGrain.cs
@@ -0,0 +1,19 @@
+﻿using DataModels;
+using Orleans;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrleansBasics
+{
+    public interface ICartGrain : IGrainWithGuidKey
+
+    {   //From which user?
+        Task<Cart> GetCart();
+
+        //From which user?
+        Task<List<Product>> GetProducts();
+
+        //To which user?
+        Task AddProduct(Product product);
+    }
+}
---
+++ Grains/CartGrain.cs
@@ -0,0 +1,26 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansBasics
+{
+    public class CartGrain : Orleans.Grain, ICartGrain
+    {
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cart> GetCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
